3395434de301e6bf4d221318d2fcf74955a69536
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 20:58:09 +0800

    Added ReportCommand

---
+++ ToyRobotSimulator.Application/Commands/ReportCommand.cs
@@ -0,0 +1,12 @@
+﻿using ToyRobotSimulator.Application.Interfaces;
+using ToyRobotSimulator.Domain.Models;
+
+namespace ToyRobotSimulator.Application.Commands;
+
+public class ReportCommand : IReport
+{
+    public string GetReport(Robot robot)
+    {
+        return null;
+    }
+}
