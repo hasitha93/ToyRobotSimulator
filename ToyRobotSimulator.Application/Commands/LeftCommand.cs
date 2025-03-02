4eab973018bf819c59cb58e0a02a0b0d64e36b0a
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 20:43:32 +0800

    Added LeftCommand

---
+++ ToyRobotSimulator.Application/Commands/LeftCommand.cs
@@ -0,0 +1,12 @@
+﻿using ToyRobotSimulator.Application.Interfaces;
+using ToyRobotSimulator.Domain.Models;
+
+namespace ToyRobotSimulator.Application.Commands;
+
+public class LeftCommand : ICommand
+{
+    public void Execute(Robot robot)
+    {
+
+    }
+}
