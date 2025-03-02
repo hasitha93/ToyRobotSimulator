8f5e4f358e66f36d8d72ee3e2998d84a59e036b1
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 20:47:39 +0800

    Added RightCommand

---
+++ ToyRobotSimulator.Application/Commands/RightCommand.cs
@@ -0,0 +1,12 @@
+﻿using ToyRobotSimulator.Application.Interfaces;
+using ToyRobotSimulator.Domain.Models;
+
+namespace ToyRobotSimulator.Application.Commands;
+
+public class RightCommandc : ICommand
+{
+    public void Execute(Robot robot)
+    {
+
+    }
+}
