070e7aa10a521f65678e768c49e44fad5b9d01a8
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 20:39:07 +0800

    Added Execute_ValidMoveNorth_RobotMoves test

---
+++ ToyRobotSimulator.Tests/Application.Tests/Commands/MoveCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using ToyRobotSimulator.Application.Commands;
+using ToyRobotSimulator.Domain.Enums;
+using ToyRobotSimulator.Domain.Models;
+
+namespace ToyRobotSimulator.Tests.Application.Tests.Commands;
+
+public class MoveCommandTests
+{
+    [Fact]
+    public void Execute_ValidMoveNorth_RobotMoves()
+    {
+        var robot = new Robot();
+        robot.Place(0, 0, Direction.NORTH);
+        var command = new MoveCommand();
+
+        command.Execute(robot);
+
+        Assert.Equal(0, robot.X);
+        Assert.Equal(1, robot.Y);
+    }
+}
