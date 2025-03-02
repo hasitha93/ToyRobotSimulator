3a3f1e14863996efde6456109485f712dc8de384
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 20:36:56 +0800

    Added MoveCommand

---
+++ ToyRobotSimulator.Application/Commands/MoveCommand.cs
@@ -0,0 +1,12 @@
+﻿using ToyRobotSimulator.Application.Interfaces;
+using ToyRobotSimulator.Domain.Models;
+
+namespace ToyRobotSimulator.Application.Commands;
+
+public class MoveCommand : ICommand
+{
+    public void Execute(Robot robot)
+    {
+    
+    }
+}
