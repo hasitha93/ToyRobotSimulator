f814d8876869d16f76d505974197c1f8e1cc1c75
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 20:58:49 +0800

    Added Execute_RobotPlaced_ReturnsCorrectPositionAndFacing test

---
+++ ToyRobotSimulator.Tests/Application.Tests/Commands/ReportCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobotSimulator.Application.Commands;
+using ToyRobotSimulator.Domain.Enums;
+using ToyRobotSimulator.Domain.Models;
+
+namespace ToyRobotSimulator.Tests.Application.Tests.Commands;
+
+public class ReportCommandTests
+{
+    [Fact]
+    public void Execute_RobotPlaced_ReturnsCorrectPositionAndFacing()
+    {
+        var robot = new Robot();
+        robot.Place(1, 2, Direction.EAST);
+        var command = new ReportCommand();
+
+        var result = command.GetReport(robot);
+
+        Assert.Equal($"1, 2, {Direction.EAST}", result);
+    }
+}
