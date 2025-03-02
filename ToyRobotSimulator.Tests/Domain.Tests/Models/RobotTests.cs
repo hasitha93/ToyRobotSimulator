8bf6a16f3d51742d312ef2bd1b926ac4b7e3b5e7
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 16:58:01 +0800

    Added Place_ValidPosition_RobotIsPlaced test case

---
+++ ToyRobotSimulator.Tests/Domain.Tests/Models/RobotTests.cs
@@ -0,0 +1,20 @@
+﻿using ToyRobotSimulator.Domain.Enums;
+using ToyRobotSimulator.Domain.Models;
+
+namespace ToyRobotSimulator.Tests.Domain.Tests.Models;
+
+public class RobotTests
+{
+    [Fact]
+    public void Place_ValidPosition_RobotIsPlaced()
+    {
+        var robot = new Robot();
+
+        robot.Place(0, 0, Direction.NORTH);
+
+        Assert.True(robot.IsPlaced);
+        Assert.Equal(0, robot.X);
+        Assert.Equal(0, robot.Y);
+        Assert.Equal(Direction.NORTH, robot.Facing);
+    }
+}
