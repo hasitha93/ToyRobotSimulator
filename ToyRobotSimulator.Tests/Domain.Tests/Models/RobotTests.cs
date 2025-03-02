df643edd41a7f7cda524d600543f80549a5794c8
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 18:12:34 +0800

    Added Place_InvalidPosition_RobotIsNotPlaced test case

--- ToyRobotSimulator.Tests/Domain.Tests/Models/RobotTests.cs
+++ ToyRobotSimulator.Tests/Domain.Tests/Models/RobotTests.cs
@@ -1,5 +1,6 @@
 ﻿using ToyRobotSimulator.Domain.Enums;
 using ToyRobotSimulator.Domain.Models;
+using Xunit;
 
 namespace ToyRobotSimulator.Tests.Domain.Tests.Models;
 
@@ -16,5 +17,17 @@ public class RobotTests
         Assert.Equal(0, robot.X);
         Assert.Equal(0, robot.Y);
         Assert.Equal(Direction.NORTH, robot.Facing);
+    }
+
+    [Theory]
+    [InlineData(-1, 5, Direction.NORTH)]
+    [InlineData(5, 6, Direction.SOUTH)]
+    public void Place_InvalidPosition_RobotIsNotPlaced(int x, int y, Direction facing)
+    {
+        var robot = new Robot();
+
+        robot.Place(x, y, facing);
+
+        Assert.False(robot.IsPlaced);
     }
 }
