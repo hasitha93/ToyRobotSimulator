7865359671142bed2da937661856a28dcbc7a80a
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 20:31:10 +0800

    Added Execute_ValidPlacement_RobotIsPlaced test

--- ToyRobotSimulator.Application/Commands/PlaceCommand.cs
+++ ToyRobotSimulator.Application/Commands/PlaceCommand.cs
@@ -1,10 +1,22 @@
 ﻿using ToyRobotSimulator.Application.Interfaces;
+using ToyRobotSimulator.Domain.Enums;
 using ToyRobotSimulator.Domain.Models;
 
 namespace ToyRobotSimulator.Application.Commands;
 
 public class PlaceCommand : ICommand
 {
+    private readonly int _x;
+    private readonly int _y;
+    private readonly Direction _facing;
+
+    public PlaceCommand(int x, int y, Direction facing)
+    {
+        _x = x;
+        _y = y;
+        _facing = facing;
+    }
+
     public void Execute(Robot robot)
     {
         
---
+++ ToyRobotSimulator.Tests/Application.Tests/Commands/PlaceCommandTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobotSimulator.Application.Commands;
+using ToyRobotSimulator.Domain.Enums;
+using ToyRobotSimulator.Domain.Models;
+
+namespace ToyRobotSimulator.Tests.Application.Tests.Commands;
+
+public class PlaceCommandTests
+{
+    [Fact]
+    public void Execute_ValidPlacement_RobotIsPlaced()
+    {
+        var robot = new Robot();
+        var command = new PlaceCommand(0, 0, Direction.NORTH);
+
+        command.Execute(robot);
+
+        Assert.True(robot.IsPlaced);
+        Assert.Equal(0, robot.X);
+        Assert.Equal(0, robot.Y);
+        Assert.Equal(Direction.NORTH, robot.Facing);
+    }
+}
