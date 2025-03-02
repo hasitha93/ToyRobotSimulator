5d2e0dde071e2bd882da6f0d5716f9dbe73b5944
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 16:50:10 +0800

    Added Robot class

---
+++ ToyRobotSimulator.Domain/Models/Robot.cs
@@ -0,0 +1,22 @@
+﻿using ToyRobotSimulator.Domain.Enums;
+
+namespace ToyRobotSimulator.Domain.Models;
+
+public class Robot
+{
+    private const int TABLE_SIZE = 5;
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public Direction Facing { get; private set; }
+    public bool IsPlaced { get; private set; }
+
+    public Robot()
+    {
+        IsPlaced = false;
+    }
+
+    public void Place(int x, int y, Direction facing)
+    {
+
+    }
+}
