1804e3aa7472e61ca5fd66529e7e41dc0245159e
Hasitha Balasinghe <hasitha.balasinghe@outlook.com>
2025-03-02 20:43:57 +0800

    Added Execute_RobotTurnsWestFromNorth test

---
+++ ToyRobotSimulator.Tests/Application.Tests/Commands/LeftCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using ToyRobotSimulator.Application.Commands;
+using ToyRobotSimulator.Domain.Enums;
+using ToyRobotSimulator.Domain.Models;
+
+namespace ToyRobotSimulator.Tests.Application.Tests.Commands;
+
+public class LeftCommandTests
+{
+    [Fact]
+    public void Execute_RobotTurnsWestFromNorth()
+    {
+        var robot = new Robot();
+        robot.Place(0, 0, Direction.NORTH);
+        var command = new LeftCommand();
+
+        command.Execute(robot);
+
+        Assert.Equal(Direction.WEST, robot.Facing);
+    }
+}
